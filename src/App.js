9773d5c6c95f3f9485075dbf8f255d8c3447eaa3
kingjamesx <talk2james.uj@gmail.com>
2022-11-25 11:48:01 +0100

    ci: add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [value, setValue] = useState(0);
+  const handleIncrement = () => {
+    setValue(value + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Team 2 Frontend App</h1>
+      <h1>{value}</h1>
+      <div>
+        <button onClick={handleIncrement}>increase</button>
+        <button>Decrease</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
